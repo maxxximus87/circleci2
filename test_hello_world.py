9bc236e81cc5d7339c57d0c1d3bd7e18eecec663
Bakar Taqeem <masmustaqeem@gmail.com>
2020-11-19 14:25:32 +0900

    first commit

---
+++ test_hello_world.py
@@ -0,0 +1,24 @@
+import hello_world
+import unittest
+import flask_circleci
+
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.app = flask_circleci.app.test_client()
+        self.app.testing = True
+
+    def test_status_code(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_buttonPressed(self):
+        ButtonPressed = "Yes"
+        response = self.app.post('/button')
+        if ButtonPressed in str(response.data):
+            self.assertEqual(ButtonPressed, ButtonPressed)
+        else:
+            self.assertEqual(response.data, ButtonPressed)
+
+if __name__ == '__main__':
+    unittest.main()
